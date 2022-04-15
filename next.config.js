37a6b2626d92747c397047eb53b3d4ff29fca852
priyankchopra2 <priyank.chopra2@gmail.com>
2022-04-16 00:01:32 +0530

    deploy app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
